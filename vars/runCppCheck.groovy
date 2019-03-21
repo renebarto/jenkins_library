085cbd303f733a6e501a843a2e23581e99231565
Rene Barto <rene.barto66@gmail.com>
2019-03-21 01:31:49 +0100

    Fix runCppCheck

--- vars/runCppCheck.groovy
+++ vars/runCppCheck.groovy
@@ -9,7 +9,14 @@ def call(String resultsDir, String resultsFile) {
   if (haveErrors(errorCode)) {
     return [errorCode, accumulatedOutput]
   }
-  (errorCode, output) = runCommand("cppcheck --enable=warning,performance,portability,style --language=c++ --xml-version=2 --inline-suppr include src test 2>${resultsDir}/${resultsFile}")
+  def outfile = "stdout.out"
+  try {
+    errorCode = sh(returnStatus: true, script: "cppcheck --enable=warning,performance,portability,style --language=c++ --xml-version=2 --inline-suppr include src test  >${outfile} 2>${resultsDir}/${resultsFile}")
+    output = readFile(outfile).trim()
+  }
+  catch (Exception ex) {
+    echo "runCommand failed with exception: '${ex}'"
+  }
   accumulatedOutput = "${accumulatedOutput}${output}"
   return [errorCode, accumulatedOutput]
 }
