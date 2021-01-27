c6139e55e58f77a502a3741aa5dc900a98050084
Rene Barto <rene.barto66@gmail.com>
2021-01-27 21:57:05 +0100

    Update xunit analyzer

--- vars/analyzeTestResults.groovy
+++ vars/analyzeTestResults.groovy
@@ -1,20 +1,6 @@
 def call(String resultsDir) {
-  xunit {
-    thresholds: [
-      failed(
-        failureThreshold: '0', 
-        unstableThreshold: '0'
-      )
-    ], 
-    tools: [
-      GoogleTest(
-        deleteOutputFiles: false, 
-        excludesPattern: '', 
-        failIfNotNew: false, 
-        pattern: "${resultsDir}/**/*.xml", 
-        stopProcessingIfError: true
-      )
-    ] 
-  }
+  xunit 
+    thresholds: [failed(failureNewThreshold: '0', failureThreshold: '0', unstableNewThreshold: '0', unstableThreshold: '0')], 
+    tools: [GoogleTest(deleteOutputFiles: false, excludesPattern: '', pattern: '"${resultsDir}/**/*.xml"', skipNoTestFiles: true, stopProcessingIfError: true)]
   return true
 }
