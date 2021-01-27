c3279b31978a3ec586ac44d944081c39ee211059
Rene Barto <rene.barto66@gmail.com>
2021-01-27 22:05:30 +0100

    Update xunit analyzer again, enable timestamps

--- vars/analyzeTestResults.groovy
+++ vars/analyzeTestResults.groovy
@@ -1,6 +1,4 @@
 def call(String resultsDir) {
   xunit 
-    thresholds: [failed(failureNewThreshold: '0', failureThreshold: '0', unstableNewThreshold: '0', unstableThreshold: '0')], 
-    tools: [GoogleTest(deleteOutputFiles: false, excludesPattern: '', pattern: '"${resultsDir}/**/*.xml"', skipNoTestFiles: true, stopProcessingIfError: true)]
-  return true
+    thresholds: [failed(failureNewThreshold: '0', failureThreshold: '0', unstableNewThreshold: '0', unstableThreshold: '0')], tools: [GoogleTest(deleteOutputFiles: false, excludesPattern: '', pattern: '"${resultsDir}/**/*.xml"', skipNoTestFiles: true, stopProcessingIfError: true)]
 }
--- vars/build_networking.groovy
+++ vars/build_networking.groovy
@@ -31,6 +31,7 @@ def call(body) {
         unit: 'HOURS'
       )
       ansiColor('xterm')
+	  timestamps ()
     }
     environment {
       errorCode = 0
